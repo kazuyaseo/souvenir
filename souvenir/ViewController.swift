956cf6fe923f2d567418460762003fffcf990656
瀬尾一矢 <seokazuya@seokazuyanoMacBook-Pro.local>
2019-06-23 19:32:47 +0900

    initial commit

---
+++ souvenir/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  souvenir
+//
+//  Created by 瀬尾一矢 on 2019/06/23.
+//  Copyright © 2019 瀬尾一矢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
